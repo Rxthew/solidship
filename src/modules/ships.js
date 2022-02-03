6d0708b1dceee6d413d49abaa2453e07de68b6e3
rxthew <richardtheumaster@gmail.com>
2022-02-03 15:35:22 +0100

    Minor change (naming)

--- src/modules/ships.js
+++ src/modules/ships.js
@@ -10,7 +10,7 @@ const _basicMethod = {
     }
 }
 
-const templateForCustomShipTypes = class {
+const _templateForCustomShipTypes = class {
     constructor(action, properties){
         this.action = action;
         this.properties = properties
@@ -22,16 +22,16 @@ const _shipMethods = {
         return Object.assign({},_basicMethod.basic)
     },
     legacy : function(){
-        return  Object.assign(new templateForCustomShipTypes(['legacy'], {messagingProtocol: 'legacy'}),_basicMethod.basic)
+        return  Object.assign(new _templateForCustomShipTypes(['legacy'], {messagingProtocol: 'legacy'}),_basicMethod.basic)
     },
     planting : function(){
-        return  Object.assign(new templateForCustomShipTypes(['plant'], {messagingProtocol: 'planting'}),_basicMethod.basic)
+        return  Object.assign(new _templateForCustomShipTypes(['seagrass planting'], {messagingProtocol: 'planting'}),_basicMethod.basic)
     },
     defense : function(){
-        return  Object.assign(new templateForCustomShipTypes(['launch decoys'], {messagingProtocol: 'defense'}),_basicMethod.basic)
+        return  Object.assign(new _templateForCustomShipTypes(['launch decoys'], {messagingProtocol: 'defense'}),_basicMethod.basic)
     },
     relay : function(){
-        return  Object.assign(new templateForCustomShipTypes(['message'], {messagingProtocol: ['message','relay']}),_basicMethod.basic)
+        return  Object.assign(new _templateForCustomShipTypes(['message'], {messagingProtocol: ['message','relay']}),_basicMethod.basic)
     },
 }
 
@@ -50,15 +50,15 @@ export const legacyShip = function(){
     return Object.assign({},_shipMethods.legacy(), new basicShip('legacy',4))
 }
 
-export const seaGrassSeedPlantingShip = function (){
+export const plantingShip = function (){
     return Object.assign({}, _shipMethods.planting(), new basicShip('planting',3))
 }
 
-export const projectileDefenseShip = function (){
+export const defenseShip = function (){
     return Object.assign({}, _shipMethods.defense(), new basicShip('defense',3))
 }
 
-export const messageRelayShip = function(){
+export const relayShip = function(){
     return Object.assign({}, _shipMethods.relay(), new basicShip('relay',3))
 }
 
