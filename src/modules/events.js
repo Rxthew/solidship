77511b9575ee1d8a63f91aece4d23ef9cfa03f7a
rxthew <richardtheumaster@gmail.com>
2022-02-07 01:27:51 +0100

    saving progress.

--- src/modules/events.js
+++ src/modules/events.js
@@ -3,9 +3,10 @@ export const events = function(){
     const _handlersObject = class{
         constructor(handler, ...args){
             this.handler = handler
-            this.arguments = [...args]
+            this.params = [...args]
 
         }
+
     }
 
     const _myEvents = {
@@ -21,13 +22,50 @@ export const events = function(){
            return Object.assign(_myEvents._events, _myEvents._events[name] = []);
        }
     }
+    
+    const _occurrenceToIndex = function(someArray, elem, sequenceOccurrence){
+        let index = 0
+         for(let occurrence = 0; occurrence < sequenceOccurrence; occurrence++){
+             let lastInd = index;
+             index = someArray.indexOf(elem,lastInd);   
+         }
+         return index
+    }
+
+    const _determineIndex = function(index, someArray, elem, sequenceOccurrence){
+        if(index || index===0){
+            return index
+        }
+        if(sequenceOccurrence === false){
+            return false
+        }
+        return _occurrenceToIndex(someArray, elem, sequenceOccurrence)
+
+    }
+
+    const _reduceHandlersObj = function(someArr){
+        const copyArr = [...someArr];
+        const newArr = copyArr.map(elem => elem.handler ? elem.handler : false)
+        return newArr;
+
+        }
 
 
-    const removeHandler = function(name, toRemove){
+    const removeHandler = function(name, handlerToRemove, index=false, sequenceOccurrence=false){
 
-        _checkEvent(name)
-            
-        _myEvents._events[name] = _myEvents._events[name].filter(elem => elem.handler !== toRemove)
+        _checkEvent(name) //separate selection routine from removal. Should be just name, handlertorm and index.
+        
+        const handlersArray = _reduceHandlersObj(_myEvents._events[name])
+        let finalIndex = _determineIndex(index,handlersArray,handlerToRemove,sequenceOccurrence)
+
+        if(finalIndex !== false){
+            _myEvents._events[name] = _myEvents._events[name].filter(elem => elem !== _myEvents._events[name][finalIndex])
+
+            return 
+        }
+
+        _myEvents._events[name] = _myEvents._events[name].filter(elem => elem.handler !== handlerToRemove)
+
         return
 
     };
@@ -37,7 +75,7 @@ export const events = function(){
         _checkEvent(name)
 
         _myEvents._events[name].forEach(function(h){
-                h.handler(...h.arguments)
+                h.handler(...h.params)
         })
         return
         
@@ -45,24 +83,32 @@ export const events = function(){
 
     const subscribe = function(name, newHandler, ...optionalArgs){
 
-        _checkEvent(name)
+        _checkEvent(name) 
 
         _myEvents._events[name] = [..._myEvents._events[name], new _handlersObject(newHandler, ...optionalArgs)] 
         return 
         
     };
 
-    const suppress = function(eventName, suppressedHandler){
+    const selectivePublish = function(eventName, ...suppressedHandlers){ 
          
-        _checkEvent(eventName)
+        _checkEvent(eventName) //use selection routine for this and update.
         
         let temporaryArray = [..._myEvents._events[eventName]]
-        temporaryArray = temporaryArray.filter(elem => elem.handler !== suppressedHandler)
+
+        for (let evHandler of suppressedHandlers){
+            temporaryArray = temporaryArray.filter(elem =>
+                elem.handler !== evHandler)
+        }
+            
+            
         temporaryArray.forEach(function(h){
-            h.handler(...h.arguments)
+            h.handler(...h.params)
         })
         return
     }
+
+    //add a subscribe 
 
 
             
@@ -70,6 +116,6 @@ export const events = function(){
         publish,
         subscribe,
         removeHandler,
-        suppress
+        selectivePublish
     }    
 }
--- src/modules/tests/events.test.js
+++ src/modules/tests/events.test.js
@@ -3,14 +3,45 @@ import {events} from '../events'
 
 const eventsInstance = events();
 
-test('custom event fires with expected result', () => {
-    let aSampleArray = []
-    let addToArray = function(someArr){
-        someArr.push(2)
-        return someArr
+let addToArray = function(someArr){
+    someArr.push(2)
+    return someArr
 
-    }
+}
+
+let addMoreToArray = function(someArr){
+    someArr.push(3)
+    return someArr
+}
+
+test('custom event calls handler', () => {
+    let aSampleArray = []
+    
     eventsInstance.subscribe('thisEvent',addToArray, aSampleArray)
     eventsInstance.publish('thisEvent')
     expect(aSampleArray).toEqual([2])
 })
+
+test('custom event calls multiples handlers in expected order', () => {
+    let aSampleArray2 = []
+
+    eventsInstance.subscribe('thisEvent2',addToArray, aSampleArray2)
+    eventsInstance.subscribe('thisEvent2',addMoreToArray, aSampleArray2)
+    eventsInstance.publish('thisEvent2')
+    expect(aSampleArray2).toEqual([2,3])
+
+})
+
+//test('remove the right handler from event', () => { Refactor
+//    let aSampleArray3 = []
+//    let addToArraySecond = addToArray
+
+//    eventsInstance.subscribe('thisEvent3',addToArray, aSampleArray3)
+//    eventsInstance.subscribe('thisEvent3',addMoreToArray, aSampleArray3)
+//    eventsInstance.subscribe('thisEvent3',addToArraySecond,aSampleArray3)
+//    eventsInstance.subscribe('thisEvent3', addMoreToArray, aSampleArray3)
+//    eventsInstance.removeHandler('thisEvent3',addToArraySecond)
+//    eventsInstance.publish('thisEvent3')
+//    expect(aSampleArray3).toEqual([3,3])
+//})
+
