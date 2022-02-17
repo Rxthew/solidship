f0ad7f82909a4113a41ea49bcb41445d7ed79932
rxthew <richardtheumaster@gmail.com>
2022-02-17 10:56:45 +0100

    fireMissile function & tests

--- src/modules/AI.js
+++ src/modules/AI.js
@@ -1,5 +1,14 @@
 import { gameBoard, createContainsObject,updateBoardContents } from "./gameboard"
 
-export const fireMissile = function(){
-
+export const fireMissile = function(key, currentBoard){
+    let loc = currentBoard.board[key].contains
+    if(loc === null){
+        return currentBoard
+    }
+    let updatedValue = Object.assign(Object.create(Object.getPrototypeOf(loc)),loc)
+    updatedValue.damage += 1;
+    let vesselDamaged = new gameBoard('vessel hit')
+    let containsObj = createContainsObject(currentBoard,key, updatedValue)
+    vesselDamaged = updateBoardContents(vesselDamaged,containsObj)
+    return vesselDamaged
 }
--- src/modules/tests/AI.test.js
+++ src/modules/tests/AI.test.js
@@ -1,26 +1,22 @@
 import { describe, expect, test } from '@jest/globals' 
-import { gameBoard, updateBoardContents } from '../gameboard'
+import { gameBoard } from '../gameboard'
 import * as ships from '../ships'
-import { fireMissle } from '../AI'
+import { fireMissile } from '../AI'
 
 describe('AI testing', () => {
     const planting = ships.plantingShip
-    let regGameBoard = new gameBoard('full')
-    let fullContainsObject = (function(){
-        const boardObj = regGameBoard.board
-        let containsObj = {}
-        for(let key of Object.keys(boardObj)){
-            containsObj[key].contains = Object.assign(containsObj, {[key]: planting()})
-        }
-        return containsObj
-    })()
-    let fullGameBoard = updateBoardContents(regGameBoard,fullContainsObject)
+    let someGameBoard = new gameBoard('some')
+    someGameBoard.board.B4.contains = planting()
+    let missileFired = fireMissile('B4', someGameBoard)
 
     test('fireMissile damages a ship if it hits it',() => {
-        //expect firemissie not to equal fullGameBoard
+        expect(missileFired.board.B4.contains.damage).toBe(1)
 
     })
     test('fireMissile returns the same gameBoard if there is no impact',() => {
-        
+        let secondMissileFired = fireMissile('A5',missileFired)
+        expect(secondMissileFired).toEqual(missileFired)
+        expect(secondMissileFired.board.A5.contains).toEqual(null)
+
     })
 })
