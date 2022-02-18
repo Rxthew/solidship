862b9b685d14870af69792257d106c1903500888
rxthew <richardtheumaster@gmail.com>
2022-02-18 17:36:24 +0100

    update re: assess damage on missile fire

--- src/modules/AI.js
+++ src/modules/AI.js
@@ -1,4 +1,5 @@
 import { gameBoard, createContainsObject,updateBoardContents } from "./gameboard"
+import { legacyShip } from "./ships"
 
 export const AIObj = class {
     constructor(gameState=new gameBoard('new game'),triangulation=false,phase=0, hit=null){
@@ -9,17 +10,29 @@ export const AIObj = class {
     }
 }
 
+const _sunkVesselCheckUpdate = function(vesselSunk, vessel){
+    if(vesselSunk){
+        return null
+    }
+    return vessel
+}
+
+const _generateDamage = function(hitShip,hitValue=1){
+     hitShip.damage += hitValue
+     return _sunkVesselCheckUpdate(hitShip.isSunk(hitShip.damage, hitShip.breakPoint), hitShip)
+}
+
 const _fireMissile = function(key, currentBoard){
     let loc = currentBoard.board[key].contains
     if(loc === null){
         return currentBoard
     }
     let updatedValue = Object.assign(Object.create(Object.getPrototypeOf(loc)),loc)
-    updatedValue.damage += 1;
-    let vesselDamaged = new gameBoard('vessel hit')
+    updatedValue = _generateDamage(updatedValue)
+    let vesselStatus = updatedValue === null ? new gameBoard('vessel sunk') : new gameBoard('vessel hit')
     let containsObj = createContainsObject(currentBoard,key, updatedValue)
-    vesselDamaged = updateBoardContents(vesselDamaged,containsObj)
-    return vesselDamaged
+    vesselStatus = updateBoardContents(vesselStatus,containsObj)
+    return vesselStatus
 }
 
 const _generatePseudoRandomKey = function(){
