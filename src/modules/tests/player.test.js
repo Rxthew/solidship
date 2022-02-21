a3c09a11cb2cf3ef91d81afd46c2d1970bd48efe
rxthew <richardtheumaster@gmail.com>
2022-02-21 17:43:22 +0100

    updated player tests to be based on PlayerReact

--- src/modules/tests/player.test.js
+++ src/modules/tests/player.test.js
@@ -1,6 +1,6 @@
 import {test,expect,describe} from "@jest/globals";
-import { gameBoard } from "../gameboard";
-import {playerObj, placeShip,moveShip} from '../player';
+import { gameBoard, } from "../gameboard";
+import {playerObj, placeShip, moveShip, playerReact} from '../player';
 import {legacyShip,plantingShip} from '../ships'
 
 
@@ -28,68 +28,75 @@ test('Able to update player gameState', () => {
     })
 })
 
-describe('testing placeShip function', () => {
-
-    let first = new gameBoard('ship placed')
-    let legacy = legacyShip()
-    let target = 'A3'
-    let newFirst = placeShip(target,first,legacy)
-
-    test('return a new gameBoard with a correctly placed ship',() => {
-        expect(newFirst.board[target].contains).toEqual(legacy)
-    })
+describe('testing playerReact function',() => {
     
-    let planting = plantingShip()
-    let anotherBoard = placeShip(target,newFirst,planting)
-
-    test('return a new gameBoard with the ship that was previously in place', () => {
-        expect(anotherBoard.board[target].contains).toEqual(legacy)
-    })
-
-    let target1 = 'A4'
-    let newFirst2 = placeShip(target1,first,null)
-
-    test('expect null when passing a \'null\' ship to an empty object', () => {
-        expect(newFirst2.board[target1].contains).toEqual(null)
-    })
-
-    let newFirst3 = placeShip(target1,newFirst2,{isSunk: 'not sunk', damage: 44, type:'non-existing' })
-
-    test('expect null when passing a ship object with missing properties', () => {
-        expect(newFirst3.board[target1].contains).toEqual(null)
-        let newFirst4 = placeShip(target1,newFirst3,{damage: 44, type: 'non-existing', breakPoint: 'someBreakPoint'})
-        expect(newFirst4.board[target1].contains).toEqual(null)
-    })
-
-})
-
-describe('testing moveShip function', () => {
-    let first = new gameBoard('place a ship')
-    let legacy = legacyShip()
-    let source = 'A5'
-
-    let second = placeShip(source,first,legacy)
-
-
-    test('Move illegal should return same gameBoard', () => {
-        expect(moveShip('F6',second,source)).toEqual(second)
-        expect(second.board['F6'].contains).toEqual(null)
-        expect(second.board[source].contains).toEqual(legacy)
+    test('Player React returns a valid gameState which returns the same gameBoard and a missile missed ship state if there is no impact',() => {
+        let aiGameBoard = new gameBoard('A1')
+        expect(playerReact(aiGameBoard).gameState.board).toEqual(aiGameBoard.board)
+        expect(playerReact(aiGameBoard).gameState.state).toBe('missile missed ship')
 
     })
-    let third = moveShip('A6',placeShip(source,second,legacy),source)
-    test('Legal move should return a new gameBoard', () => {
+
+    test('Player React returns a valid gameState which reflects damage done to ship and a missile hit ship state if ship hit',() => {
+        let aiGameBoard2 = new gameBoard('C2')
+        aiGameBoard2.board['C2'].contains = plantingShip()
+
+        expect(playerReact(aiGameBoard2).gameState.board['C2'].contains.damage).toBe(1)
+        expect(playerReact(aiGameBoard2).gameState.state).toBe('missile hit ship')
+
+    })
+
+    test('Player React returns a valid gameState which replaces ship with null and a missile sunk ship state if ship sinks from a hit',() => {
+        let aiGameBoard3 = new gameBoard('B3')
+        let plant = plantingShip()
+        plant.damage = plant.breakPoint - 1
+        aiGameBoard3.board['B3'].contains = plant
+        
+        expect(playerReact(aiGameBoard3).gameState.board['B3']).toBe(null)
+        expect(playerReact(aiGameBoard3).gameState.state).toBe('missile sunk ship')
+
+    })
+
+
+    test('if PlayerReact is passed placeShip function returns a new gameboard with a correctly placed ship (+ state)', () => {
+        let first = new gameBoard('ship placed')
+        let legacy = legacyShip()
+        let target = 'A3'
+        let newReact = playerReact(placeShip(target,first,legacy)) 
+
+        expect(newReact.gameState.board[target].contains).toEqual(legacy)
+
+    })
+
+    test('if PlayerReact is passed moveShip function in a legal move returns a new gameBoard with newly positioned ship',() => {
+        let first = new gameBoard('place a ship')
+        let legacy = legacyShip()
+        let source = 'A5'
+    
+        let second = placeShip(source,first,legacy)
+        let third = moveShip('A6',placeShip(source,second,legacy),source)
+        
         expect(third.board['A6'].contains).toEqual(legacy)
         expect(third.board[source].contains).toEqual(null)
-    })
-    let fourth = moveShip('A5',third,'A6')
-    test('move back returns previous results',() => {
+        
+        let fourth = moveShip('A5',third,'A6')
+        
         expect(fourth.board['A6'].contains).toEqual(null)
         expect(fourth.board['A5'].contains).toEqual(legacy)
+        
+
     })
-    
-    
+
+
+    test(
+        'Key in board state passed in Player React corresponds to a key/legal moves of key where a defense ship is, and missileBlock func passed in. Expect same board with missile blocked state', () => {
+        
+        
+            })
 })
+
+
+
 
 
 
