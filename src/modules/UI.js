2399308db9422abf10f3c945e01a19a8e5d295e0
rxthew <richardtheumaster@gmail.com>
2022-04-18 22:45:49 +0200

    reorganisation of code & broadened helpers' scope

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,18 +2,6 @@ import { defaultConfig} from './gameboard.js'
 import { gameEvents } from './gamestate.js'
 import * as ships from './ships.js'
 
-
-const standardShipStore = {
-    'Basic' : function(){return new ships.basicShip()},
-    'Basic (Legacy)' : function(){return new ships.basicLegacyShip()},
-    'Legacy' : ships.legacyShip,
-    'Planting' : ships.plantingShip,
-    'Defense' : ships.defenseShip,
-    'Relay' : ships.relayShip,
-    'Clearing' : ships.clearingShip
-}
-
-
 const _buildBoard = function(){
     const lets = ['A','B','C','D','E','F']
     const nums = ['1','2','3','4','5','6']
@@ -57,33 +45,6 @@ const createMainConsole = function(){
 
 }
 
-
-const _shipStore = function(shipsObj=standardShipStore){
-    let store = document.createElement('div')
-    store.classList.add('shipStore')
-    let choices = Object.keys(shipsObj)
-    for (let elem of choices){
-        let ship = document.createElement('span')
-        ship.textContent = elem
-        ship.classList.add('shipOption')
-        //ship.onclick = when clicked, series of happenings: first initialises the object value associated with key chosen.
-        // Then prompts the user to click on an empty place in the grid to place the ship on. Then re-renders the gameboard
-        //and the console. Also: if user fails to click on an empty grid item, user is prompted with an error and the next
-        // click re renders everything. If user fails to click in the gameboard and click anywhere else: re-render everything. 
-        store.appendChild(ship) 
-    }
-
-}
-
-
-const _componentStore = function(componentsObj=ships.components, path){
-    let store = document.createElement('div')
-    store.classList.add('componentStore')
-    let choices = Object.keys(componentsObj())
-
-    //remember componentsObj still need to initialise. 
-    
-}
 
 const recordPathHelpers = function(){
 
@@ -134,17 +95,17 @@ const recordPathHelpers = function(){
             return  confirmedPaths 
     }
 
-    let chartPath = function(event){
+    let chartPath = function(event, initParent='viewConsole', propTitle='propertyTitle'){
         let parent = event.target.parentElement
         let prop = event.target.textContent
         let path = [prop]
-        while(parent !== document.querySelector('.viewConsole')){
+        while(parent !== document.querySelector(`.${initParent}`)){
             if(parent === document.querySelector('body')){
                 return
             }
             parent = parent.parentElement
             let children = Array.from(parent.children)
-            children = children.filter(child => child.classList.contains('propertyTitle'))
+            children = children.filter(child => child.classList.contains(`${propTitle}`))
             if(children.length === 0){
                 return path
             } 
@@ -162,7 +123,170 @@ const recordPathHelpers = function(){
 }
 
 
+const _createViewConsole = function(){
+    if(document.querySelector('.viewConsole')){
+        document.querySelector('.viewConsole').remove()
+    }
+    let viewConsole = document.createElement('div')
+    viewConsole.classList.add('viewConsole')
+    document.querySelector('.mainConsole').appendChild(viewConsole) 
+    return document.querySelector('.viewConsole')
 
+}
+
+
+const _recordPaths = function(someGb,key, someGetCont=defaultConfig.getBoardContains){
+    let paths = []
+    let confirmedPaths = []
+    let obj = someGetCont(someGb,key)
+    
+    
+    let initialInjection = recordPathHelpers().initialInjection
+    initialInjection(obj,paths)
+
+    confirmedPaths = recordPathHelpers().finalisePath(obj,paths,confirmedPaths)
+    return confirmedPaths
+}
+
+const revealProps = {
+    determineUIKey : function(parent, key, propName='property',propTitleName='propertyTitle'){
+        let children = Array.from(parent.children)
+        for(let child of children){
+            if(child.classList.contains(key)){
+                return child
+            }
+        }
+        return this.keyToUIelement(parent,key,propName,propTitleName)
+
+
+    },
+    keyToUIelement : function(parent,key,propName='property',propTitleName='propertyTitle'){
+        let prop = document.createElement('div')
+        let title = document.createElement('span')
+        prop.classList.add(`${propName}`)
+        prop.classList.add(`${key}`)
+        title.classList.add(`${propTitleName}`)        
+        title.textContent = key 
+        prop.appendChild(title)
+        parent.appendChild(prop)
+        return prop
+        },
+    valueToUIelement : function(par,val,valContainerName='container',valName='element'){
+        if(Array.isArray(val)){
+            let container = document.createElement('div')
+            container.classList.add(`${valContainerName}`)
+            for(let elem of val){
+                let uiElement = document.createElement('span')
+                uiElement.classList.add(`${valName}`)
+                uiElement.textContent = elem
+                container.appendChild(uiElement)
+            }
+            par.appendChild(container)
+        }
+        else if(typeof val === 'string' || typeof val === 'number'){
+            let uiElement = document.createElement('span')
+            uiElement.classList.add(`${valName}`)
+            uiElement.textContent = val
+            par.appendChild(uiElement)
+        }
+        return
+    },        
+}
+      
+
+const displayShip = function(event, someGb, someGetCont=defaultConfig.getBoardContains){
+    let key = event.target.id
+    let paths = _recordPaths(someGb, key,someGetCont)
+    let viewConsole = _createViewConsole()
+    let shipObj = someGetCont(someGb,key)
+    
+    for(let path of paths){
+        let finalTarget = shipObj
+        let parent = viewConsole
+        for (let elem of path){
+            if(elem === path[path.length - 1]){
+                parent = revealProps.determineUIKey(parent,elem)
+                let value = finalTarget[elem]
+                revealProps.valueToUIelement(parent,value)
+            }
+            else {
+                parent = revealProps.determineUIKey(parent,elem)
+            }
+            finalTarget = finalTarget[elem]
+        }
+    }
+    return viewConsole
+    
+}
+
+const standardShipStore = {
+    'Basic' : function(){return new ships.basicShip()},
+    'Basic (Legacy)' : function(){return new ships.basicLegacyShip()},
+    'Legacy' : ships.legacyShip,
+    'Planting' : ships.plantingShip,
+    'Defense' : ships.defenseShip,
+    'Relay' : ships.relayShip,
+    'Clearing' : ships.clearingShip
+}
+
+const _shipStore = function(shipsObj=standardShipStore){
+    let store = document.createElement('div')
+    store.classList.add('shipStore')
+    let choices = Object.keys(shipsObj)
+    for (let elem of choices){
+        let ship = document.createElement('span')
+        ship.textContent = elem
+        ship.classList.add('shipOption')
+        //ship.onclick = when clicked, series of happenings: first initialises the object value associated with key chosen.
+        // Then prompts the user to click on an empty place in the grid to place the ship on. Then re-renders the gameboard
+        //and the console. Also: if user fails to click on an empty grid item, user is prompted with an error and the next
+        // click re renders everything. If user fails to click in the gameboard and click anywhere else: re-render everything. 
+        store.appendChild(ship) 
+    }
+
+}
+
+const _recordComponentPaths = function(obj){
+    let paths = []
+    let confirmedPaths = []
+
+    let initialInjection = recordPathHelpers().initialInjection
+    initialInjection(obj,paths)
+
+    confirmedPaths = recordPathHelpers().finalisePath(obj,paths,confirmedPaths)
+    return confirmedPaths
+
+}
+
+
+
+const _componentStore = function(componentsObj=ships.components,path=_recordComponentPaths(componentsObj)){
+    if(document.querySelector('.optConsole')){
+        document.querySelector('.optConsole').remove()
+    }
+    let store = document.createElement('div')
+    store.classList.add('componentStore')
+    let main = document.querySelector('.mainConsole')
+    for(let iteration of path){
+        let finalTarget = componentsObj()
+        let parent = store
+        for (let elem of iteration){
+            if(elem === iteration[iteration.length - 1]){
+                parent = revealProps.determineUIKey(parent,elem,'compProperty','compPropertyTitle')
+                let value = finalTarget[elem]
+                revealProps.valueToUIelement(parent,value,'compContainer','compElement')
+            }
+            else {
+                parent = revealProps.determineUIKey(parent,elem, 'compProperty','compPropertyTitle')
+            }
+            finalTarget = finalTarget[elem]
+        }
+    
+    }
+    main.appendChild(store)
+
+     
+}
 
 
 const _finaliseModify = function(event){
@@ -176,6 +300,7 @@ const _finaliseExtendComp = function(event){
 }
 
 const _finaliseExtend = function(event){
+    
 
 }
 
@@ -205,7 +330,7 @@ const optionsObject = {
         'Extend Ship' : function(){
 
         },
-        'Add to Ship' : function(){
+        'Extend Component' : function(){
 
          },
         'Effect Ship Action' : function(event, someGb, someGetCont=defaultConfig.getBoardContains){
@@ -250,104 +375,6 @@ const createOptionsConsole = function(...params){
         }
     }
 
-}
-
-
-
-const _createViewConsole = function(){
-    if(document.querySelector('.viewConsole')){
-        document.querySelector('.viewConsole').remove()
-    }
-    let viewConsole = document.createElement('div')
-    viewConsole.classList.add('viewConsole')
-    document.querySelector('.mainConsole').appendChild(viewConsole) 
-    return document.querySelector('.viewConsole')
-
-}
-
-
-const _recordPaths = function(someGb,key, someGetCont=defaultConfig.getBoardContains){
-    let paths = []
-    let confirmedPaths = []
-    let obj = someGetCont(someGb,key)
-    
-    
-    let initialInjection = recordPathHelpers().initialInjection
-    initialInjection(obj,paths)
-
-    confirmedPaths = recordPathHelpers().finalisePath(obj,paths,confirmedPaths)
-    return confirmedPaths
-}
-
-const revealShipProps = {
-    determineUIKey : function(parent, key){
-        let children = Array.from(parent.children)
-        for(let child of children){
-            if(child.classList.contains(key)){
-                return child
-            }
-        }
-        return this.keyToUIelement(parent,key)
-
-
-    },
-    keyToUIelement : function(parent,key){
-        let prop = document.createElement('div')
-        let title = document.createElement('span')
-        prop.classList.add('property')
-        prop.classList.add(`${key}`)
-        title.classList.add('propertyTitle')        
-        title.textContent = key 
-        prop.appendChild(title)
-        parent.appendChild(prop)
-        return prop
-        },
-    valueToUIelement : function(par,val){
-        if(Array.isArray(val)){
-            let container = document.createElement('div')
-            container.classList.add('container')
-            for(let elem of val){
-                let uiElement = document.createElement('span')
-                uiElement.classList.add('element')
-                uiElement.textContent = elem
-                container.appendChild(uiElement)
-            }
-            par.appendChild(container)
-        }
-        else if(typeof val === 'string' || typeof val === 'number'){
-            let uiElement = document.createElement('span')
-            uiElement.classList.add('element')
-            uiElement.textContent = val
-            par.appendChild(uiElement)
-        }
-        return
-    },        
-}
-      
-
-const displayShip = function(event, someGb, someGetCont=defaultConfig.getBoardContains){
-    let key = event.target.id
-    let paths = _recordPaths(someGb, key,someGetCont)
-    let viewConsole = _createViewConsole()
-    let shipObj = someGetCont(someGb,key)
-    
-    for(let path of paths){
-        let finalTarget = shipObj
-        let parent = viewConsole
-        for (let elem of path){
-            if(elem === path[path.length - 1]){
-                parent = revealShipProps.determineUIKey(parent,elem)
-                let value = finalTarget[elem]
-                revealShipProps.valueToUIelement(parent,value)
-            }
-            else {
-                parent = revealShipProps.determineUIKey(parent,elem)
-            }
-            finalTarget = finalTarget[elem]
-        }
-    }
-    return viewConsole
-    
 }
 
 
