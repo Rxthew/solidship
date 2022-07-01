530d5c2074ceb76d10083f3d2ef23155fa941d2a
rxthew <richardtheumaster@gmail.com>
2022-07-01 16:33:31 +0200

    Move numeralPropStatus & revealProps to utils

--- src/modules/UI modules/UIShip.js
+++ src/modules/UI modules/UIShip.js
@@ -1,6 +1,6 @@
 import recordPathHelpers from 'paths.js'
 import { defaultConfig } from '../gameboard'
-import { camelPhraseParser } from '../utils'
+import { revealProps, numeralPropStatus } from '../utils'
 import {finishers} from './consoles'
 
 const recordPaths = function(someGb,key,someGetCont=defaultConfig.getBoardContains){
@@ -16,62 +16,6 @@ const recordPaths = function(someGb,key,someGetCont=defaultConfig.getBoardContai
     return confirmedPaths
 }
 
-const revealProps = {
-    determineUIKey : function(parent, key, propName='property',propTitleName='propertyTitle'){
-        let children = Array.from(parent.children)
-        for(let child of children){
-            if(child.classList.contains(key)){
-                return child
-            }
-        }
-        return this.keyToUIelement(parent,key,propName,propTitleName)
-
-
-    },
-    keyToUIelement : function(parent,key,propName='property',propTitleName='propertyTitle'){
-        let prop = document.createElement('div')
-        let title = document.createElement('span')
-        prop.classList.add(`${propName}`)
-        prop.classList.add(`${key}`)
-        title.classList.add(`${propTitleName}`)
-        title.id = key        
-        title.textContent = camelPhraseParser(key) 
-        prop.appendChild(title)
-        parent.appendChild(prop)
-        return prop
-        },
-    valueToUIelement : function(par,val,valContainerName='container',valName='element'){
-        if(Array.isArray(val)){ 
-            let container = document.createElement('div')
-            container.classList.add(`${valContainerName}`)
-            for(let elem of val){
-                let uiElement = document.createElement('span')
-                uiElement.classList.add(`${valName}`)
-                uiElement.id = elem
-                uiElement.textContent = elem
-                container.appendChild(uiElement)
-            }
-            par.appendChild(container)
-            return container
-        }
-        else if(typeof val === 'string'){
-            let uiElement = document.createElement('span')
-            uiElement.classList.add(`${valName}`)
-            uiElement.id = val
-            uiElement.textContent = camelPhraseParser(val)
-            par.appendChild(uiElement)
-            return uiElement
-        }
-        else if(typeof val === 'number'){
-            let uiElement = document.createElement('span')
-            uiElement.classList.add(`numeral`)
-            uiElement.textContent = camelPhraseParser(val)
-            par.appendChild(uiElement)
-            return uiElement
-        }
-        
-    },        
-}
 
 const primaryMarker = function(par){
     let container = Array.from(par.children).filter(child => child.classList.contains('container'))
@@ -84,20 +28,6 @@ const primaryMarker = function(par){
         }   
     }
     return
-}
-
-const numeralPropStatus = function(val,par,titleType='propertyTitle',parType='property', parNewType='element'){
-    let ruledOut = ['damage','breakpoint','reinforcedBreakpoint']
-    if(val.classList.contains('numeral')){
-        let title = Array.from(par.children).filter(sib => sib.classList.contains(`${titleType}`))[0]
-        if(!ruledOut.includes(title.id)){
-            par.classList.remove(`${parType}`)
-            par.classList.add(`${parNewType}`)
-            title.classList.remove(`${titleType}`)
-            title.classList.add('countTitle')
-        }
-    }
-
 }
                     
 
--- src/modules/utils.js
+++ src/modules/utils.js
@@ -57,6 +57,20 @@ const fromNormToCamel = function(str){
 
 }
 
+export const numeralPropStatus = function(val,par,titleType='propertyTitle',parType='property', parNewType='element'){
+    let ruledOut = ['damage','breakpoint','reinforcedBreakpoint']
+    if(val.classList.contains('numeral')){
+        let title = Array.from(par.children).filter(sib => sib.classList.contains(`${titleType}`))[0]
+        if(!ruledOut.includes(title.id)){
+            par.classList.remove(`${parType}`)
+            par.classList.add(`${parNewType}`)
+            title.classList.remove(`${titleType}`)
+            title.classList.add('countTitle')
+        }
+    }
+
+}
+
 export const integrateChild = function(parent,child){
     parent.appendChild(child)
     return {
@@ -73,6 +87,63 @@ export const renderImage = async function(node,assetName){
     catch(error){
         console.log(error)
     }
+}
+
+export const revealProps = {
+    determineUIKey : function(parent, key, propName='property',propTitleName='propertyTitle'){
+        let children = Array.from(parent.children)
+        for(let child of children){
+            if(child.classList.contains(key)){
+                return child
+            }
+        }
+        return this.keyToUIelement(parent,key,propName,propTitleName)
+
+
+    },
+    keyToUIelement : function(parent,key,propName='property',propTitleName='propertyTitle'){
+        let prop = document.createElement('div')
+        let title = document.createElement('span')
+        prop.classList.add(`${propName}`)
+        prop.classList.add(`${key}`)
+        title.classList.add(`${propTitleName}`)
+        title.id = key        
+        title.textContent = camelPhraseParser(key) 
+        prop.appendChild(title)
+        parent.appendChild(prop)
+        return prop
+        },
+    valueToUIelement : function(par,val,valContainerName='container',valName='element'){
+        if(Array.isArray(val)){ 
+            let container = document.createElement('div')
+            container.classList.add(`${valContainerName}`)
+            for(let elem of val){
+                let uiElement = document.createElement('span')
+                uiElement.classList.add(`${valName}`)
+                uiElement.id = elem
+                uiElement.textContent = elem
+                container.appendChild(uiElement)
+            }
+            par.appendChild(container)
+            return container
+        }
+        else if(typeof val === 'string'){
+            let uiElement = document.createElement('span')
+            uiElement.classList.add(`${valName}`)
+            uiElement.id = val
+            uiElement.textContent = camelPhraseParser(val)
+            par.appendChild(uiElement)
+            return uiElement
+        }
+        else if(typeof val === 'number'){
+            let uiElement = document.createElement('span')
+            uiElement.classList.add(`numeral`)
+            uiElement.textContent = camelPhraseParser(val)
+            par.appendChild(uiElement)
+            return uiElement
+        }
+        
+    },        
 }
 
 
